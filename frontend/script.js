cf4429b68aebca171b199c2ccf2b4b59dfb8b3cc
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-04-12 17:48:22 +0200

    select

--- frontend/script.js
+++ frontend/script.js
@@ -3,10 +3,32 @@ new Vue({
 
     data: {
         arrayAlbums: [],
+        genereSelezionato: "general",
     },
 
     created() {
         axios.get('http://localhost:81/php-ajax-dischi/database/data.php')
             .then(response => this.arrayAlbums = response.data)
-    }
+    },
+
+    computed: {
+        funzioneCambioGenere() {
+            let arrayFiltrato = [];
+            if (this.genereSelezionato == "") {
+                arrayFiltrato = this.arrayAlbums;
+            } else if (this.genereSelezionato == "general") {
+                arrayFiltrato = this.arrayAlbums;
+            } else {
+                arrayFiltrato = this.arrayAlbums.filter((elemento) => {
+                    if (
+                        elemento.genre.toLowerCase() == this.genereSelezionato.toLowerCase()
+                    ) {
+                        return elemento;
+                    }
+                });
+            }
+
+            return arrayFiltrato;
+        },
+    },
 })
